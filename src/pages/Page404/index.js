cf5819fbb83f5a885f263d9734d3ea4ab27a88b8
Thiago <thiagodsmarcelino.com>
2022-12-16 22:48:58 -0300

    Updates

--- src/pages/Page404/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Err } from './styled';
-
-export default function Page404() {
-    return (<Err>Desculpe essa página não existe</Err>);
-}
--- src/pages/Page404/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Err = styled.h1`
-    font-size: 3rem;
-    text-align: center;
-    margin-top: 20%;
-    margin-bottom: 30%;
-`
